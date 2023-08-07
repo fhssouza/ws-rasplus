184355aba76ab692c1a48015e10b4f9483140da9
Fábio Souza <fhssouza@gmail.com>
2023-08-07 19:35:27 -0300

    Implementando os models do projeto

---
+++ src/main/java/com/client/ws/rasmooplus/model/SubscriptionsType.java
@@ -0,0 +1,37 @@
+package com.client.ws.rasmooplus.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "subscriptions_type")
+public class SubscriptionsType implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "subscriptions_type_id")
+    private Long id;
+
+    private String name;
+
+    @Column(name = "access_months")
+    private Long accessMonth;
+
+    private BigDecimal price;
+
+    @Column(name = "product_key")
+    private String productKey;
+
+
+}
